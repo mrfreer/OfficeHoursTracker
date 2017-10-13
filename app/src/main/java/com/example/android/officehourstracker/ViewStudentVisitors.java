a89b8070dc1216491b749465eced0217c4351643
Dave <david.freer@gmail.com>
2017-10-13 17:23:04 -0400

    Working on ListView

--- app/src/main/java/com/example/android/officehourstracker/ViewStudentVisitors.java
+++ app/src/main/java/com/example/android/officehourstracker/ViewStudentVisitors.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -16,19 +18,25 @@ import java.util.List;
 
 public class ViewStudentVisitors extends AppCompatActivity {
 
-
+    private RecyclerView recyclerView;
+    private List<Student> students;
+    private RecyclerView.Adapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_student_visitors);
-        ArrayList<String> arrayList = readData();
-        Toast.makeText(getApplicationContext(), arrayList.get(0).toString(), Toast.LENGTH_LONG).show();
+        ArrayList<StudentTime> arrayList = readData();
+        adapter = new AdapterStudentTime(arrayList, this);
+        recyclerView = (RecyclerView) findViewById(R.id);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
 
     }
 
-    public ArrayList<String> readData(){
+    public ArrayList<StudentTime> readData(){
         Log.i("WRITING_MDC", " READING DATABASE");
         SQLiteDatabase db = new ClassListDbHelper(this).getReadableDatabase();
         String [] projection = {
@@ -39,11 +47,11 @@ public class ViewStudentVisitors extends AppCompatActivity {
         Cursor cursor = db.query(StudentTimesDB.TABLE_NAME, projection, StudentTimesDB.COLUMN_NAME_STUDENT_ID + ">0",
                 null, null, null, null);
         List itemIds = new ArrayList<>();
-        ArrayList<String> studentNamesTimes = new ArrayList<>();
+        ArrayList<StudentTime> studentNamesTimes = new ArrayList<>();
         while(cursor.moveToNext()){
             String id = cursor.getString(cursor.getColumnIndexOrThrow(StudentTimesDB.COLUMN_NAME_STUDENT_ID));
             String time = cursor.getString(cursor.getColumnIndexOrThrow(StudentTimesDB.COLUMN_TIMESTAMP));
-            studentNamesTimes.add(id + "\n" + time + "\n");
+            studentNamesTimes.add(new StudentTime(id, time));
         }
         cursor.close();
         Log.i("testing_testing", studentNamesTimes.size() + "");
